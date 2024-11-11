53e53a09754ed3ec2e3d8a0001cbf6c5977c12fe
ilyaMr <ilya112006@yandex.ru>
2024-11-11 21:49:52 +0300

    create XCode project

---
+++ EDS_Store/EDS_StoreTests/EDS_StoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  EDS_StoreTests.swift
+//  EDS_StoreTests
+//
+//  Created by Илья Морозов on 11.11.2024.
+//
+
+import Testing
+@testable import EDS_Store
+
+struct EDS_StoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
