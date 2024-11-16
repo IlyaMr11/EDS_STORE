9bc7a357ba84b2844afdbdb42d34ee83bc3eeef4
ilyaMr <ilya112006@yandex.ru>
2024-11-16 17:13:13 +0300

    add buttons and fields

--- EDS_Store/EDS_Store/SignViewController.swift
+++ EDS_Store/EDS_Store/SignViewController.swift
@@ -12,6 +12,7 @@ class   SignViewController: UIViewController {
     //MARK: - CONSTANTS
     //radiuses
     let borderRadius = CGFloat(15)
+    let buttonsRadius = CGFloat(13)
     
     //MARK: - LOGO
     private lazy var logoImageView: UIImageView = {
@@ -41,6 +42,79 @@ class   SignViewController: UIViewController {
         return view
     }()
     
+    //MARK: TextFields
+    private lazy var loginTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Логин"
+        textField.borderStyle = .none
+        textField.layer.cornerRadius = buttonsRadius
+        textField.layer.borderWidth = 1.5
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.keyboardType = .emailAddress
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Пароль"
+        textField.borderStyle = .none
+        textField.layer.cornerRadius = buttonsRadius
+        textField.layer.borderWidth = 1.5
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.isSecureTextEntry = true
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
+    
+    //MARK: - BUTTONS
+    private lazy var siginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Войти", for: .normal)
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = buttonsRadius
+        return button
+    }()
+    
+    private lazy var registrationButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Зарегистрироваться", for: .normal)
+        button.backgroundColor = .orange
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = buttonsRadius
+        return button
+    }()
+    
+    private lazy var orLabel: UILabel = {
+        let label = UILabel()
+        label.text = "или"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    //MARK: - StackViews
+    private lazy var fieldsStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [loginTextField, passwordTextField])
+        stackView.axis = .vertical
+        stackView.spacing = view.bounds.width / 30
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [siginButton, orLabel, registrationButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Вход"
@@ -53,6 +127,8 @@ class   SignViewController: UIViewController {
         setupLogo(logoImageView)
         setupWelcomLabel(welcomeLabel)
         setupBorder(borderView)
+        setupFieldsStackView(fieldsStackView)
+        setupButtonsStackView(buttonsStackView)
     }
     func setupLogo(_ imageView: UIImageView) {
         view.addSubview(imageView)
@@ -77,6 +153,23 @@ class   SignViewController: UIViewController {
         border.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         border.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.8).isActive = true
     }
+    
+    func setupFieldsStackView(_ stackView: UIStackView) {
+        view.addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: borderView.topAnchor, constant: 10).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: borderView.bottomAnchor, constant: -view.bounds.height * 0.3).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.72).isActive = true
+    }
+    
+    func setupButtonsStackView(_ stackView: UIStackView) {
+        view.addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: fieldsStackView.bottomAnchor, constant: view.bounds.height * 0.05).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.72).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: borderView.bottomAnchor, constant: -10).isActive = true
+    }
+    
     
     @objc func toRegistration() {
         let registrationViewController = RegistrationViewController()
