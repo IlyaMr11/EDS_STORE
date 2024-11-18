68549c877733c8f39d6fe131a77f20c3c802d443
ilyaMr <ilya112006@yandex.ru>
2024-11-18 11:30:30 +0300

    realise back for uiObjects

---
+++ EDS_Store/EDS_Store/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  EDS_Store
+//
+//  Created by Илья Морозов on 18.11.2024.
+//
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Profile"
+    }
+}
--- EDS_Store/EDS_Store/SignViewController.swift
+++ EDS_Store/EDS_Store/SignViewController.swift
@@ -14,6 +14,16 @@ class   SignViewController: UIViewController {
     let borderRadius = CGFloat(15)
     let buttonsRadius = CGFloat(13)
     
+    //regex for password
+    let pasRegex1 = try! NSRegularExpression(pattern: "[0-9]+")
+    let pasRegex2 = try! NSRegularExpression(pattern: "[a-z]+")
+    let pasRegex3 = try! NSRegularExpression(pattern: "[A-Z]+")
+    let pasRegex4 = try! NSRegularExpression(pattern: "[!+?.,#-]+")
+    
+    //regex for login
+    let loginRegex = try! NSRegularExpression(pattern: "[a-zA-z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+")
+    
+    
     //MARK: - LOGO
     private lazy var logoImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "logo7"))
@@ -33,6 +43,7 @@ class   SignViewController: UIViewController {
         return label
     }()
     
+    //MARK: BORDER
     private lazy var borderView: UIView = {
         let view = UIView()
         view.layer.borderWidth = 2.5
@@ -52,6 +63,7 @@ class   SignViewController: UIViewController {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.keyboardType = .emailAddress
         textField.clearButtonMode = .whileEditing
+        textField.delegate = self
         return textField
     }()
     
@@ -64,6 +76,7 @@ class   SignViewController: UIViewController {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.isSecureTextEntry = true
         textField.clearButtonMode = .whileEditing
+        textField.delegate = self
         return textField
     }()
     
@@ -74,6 +87,7 @@ class   SignViewController: UIViewController {
         button.backgroundColor = .black
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = buttonsRadius
+        button.addTarget(self, action: #selector(toProfile), for: .touchUpInside)
         return button
     }()
     
@@ -87,6 +101,7 @@ class   SignViewController: UIViewController {
         return button
     }()
     
+    //MARK: - или LABEL
     private lazy var orLabel: UILabel = {
         let label = UILabel()
         label.text = "или"
@@ -131,6 +146,8 @@ class   SignViewController: UIViewController {
         setupFieldsStackView(fieldsStackView)
         setupButtonsStackView(buttonsStackView)
     }
+    
+    //MARK: - SETUP LOGO
     func setupLogo(_ imageView: UIImageView) {
         view.addSubview(imageView)
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -139,6 +156,7 @@ class   SignViewController: UIViewController {
         imageView.heightAnchor.constraint(equalToConstant: 120).isActive = true
     }
     
+    //MARK: SETUP WELCOM
     func setupWelcomLabel(_ label: UILabel) {
         view.addSubview(label)
         label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -view.bounds.width * 0.2).isActive = true
@@ -155,6 +173,7 @@ class   SignViewController: UIViewController {
         border.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.825).isActive = true
     }
     
+    //MARK: SETUP StackViews
     func setupFieldsStackView(_ stackView: UIStackView) {
         view.addSubview(stackView)
         stackView.topAnchor.constraint(equalTo: borderView.topAnchor, constant: 10).isActive = true
@@ -172,11 +191,65 @@ class   SignViewController: UIViewController {
     }
     
     
+    //MARK: - TARGETS
     @objc func toRegistration() {
         let registrationViewController = RegistrationViewController()
         self.navigationController?.pushViewController(registrationViewController, animated: true)
     }
     
+    @objc func toProfile() {
+        let b1 = checkLogin(loginTextField.text)
+        let b2 = checkPassword(passwordTextField.text)
+        if b1 && b2 {
+            let profileViewController = ProfileViewController()
+            self.navigationController?.pushViewController(profileViewController, animated: true)
+        }
+    }
+    //MARK: - CHECK IS CORRECT?
+    func checkPassword(_ text: String?) -> Bool {
+        guard let text = text else { return false }
+        let passordRegexes = [pasRegex1, pasRegex2, pasRegex3, pasRegex4]
+        let range = NSRange(location: 0, length: text.utf16.count)
+        for regex in passordRegexes {
+            if !(regex.firstMatch(in: text, options: [], range: range) != nil) {
+                showPassordAlert()
+                return false
+            }
+        }
+        return true
+    }
     
+    func checkLogin(_ text: String?) -> Bool {
+        guard let text = text else { return false }
+        let range = NSRange(location: 0, length: text.utf16.count)
+        if !(loginRegex.firstMatch(in: text, range: range) != nil) {
+            showLoginALert()
+            return false
+        }
+        return true
+    }
+    
+    //MARK: - SHOW ALERTS
+    func showPassordAlert() {
+        let alert = UIAlertController(title: "Неккоректный пароль", message: "Попробуйте новый пароль", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    func showLoginALert() {
+        let alert = UIAlertController(title: "Неверный логин", message: "Попробуйте другой логин", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
 }
 
+//EXTENSION
+extension SignViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
